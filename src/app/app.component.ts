ef124994b659c9f9024fbbf92100a8283f02621c
sheeban <sheeban@gooru.org>
2018-06-26 20:35:44 +0530

    Removed unused variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,7 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { TranslateService } from '@ngx-translate/core';
-import {
-  WelcomePage,
-  ProficiencyPage,
-  DashboardPage,
-  ContactUsPage,
-  JoinClassroomPage
-} from '../pages';
+import { WelcomePage, ProficiencyPage, DashboardPage } from '../pages';
 
 @Component({
   templateUrl: 'app.html'
