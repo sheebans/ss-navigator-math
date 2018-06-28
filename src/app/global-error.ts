97ba649155faeb6e2edbcca01c4917ce3aaa0b00
sheeban <sheeban@gooru.org>
2018-06-28 20:19:10 +0530

    Added test toast for error message

--- src/app/global-error.ts
+++ src/app/global-error.ts
@@ -1,14 +1,25 @@
+import { ToastController } from 'ionic-angular';
 import { ErrorHandler, Injectable } from '@angular/core';
 import { Device } from '@ionic-native/device';
 
 @Injectable()
 export class GlobalErrorHandler extends ErrorHandler {
-  constructor(private device: Device) {
+  constructor(private device: Device, private toastCtrl: ToastController) {
     super();
   }
 
   handleError(error: any): void {
     console.log(error);
     console.log(this.device);
+    this.presentToast();
+  }
+
+  presentToast() {
+    let toast = this.toastCtrl.create({
+      message: 'Need to update proper error message',
+      duration: 3000,
+      position: 'top'
+    });
+    toast.present();
   }
 }
